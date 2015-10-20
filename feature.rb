fc0305dd6990946b84212db851573fed1519d6da
mongolianprincess <ayc.yang@gmail.com>
2015-10-20 12:09:39 +0100

    Added deduct method - ch.07

--- feature.rb
+++ feature.rb
@@ -3,3 +3,4 @@ require './lib/oystercard'
 card = Oystercard.new
 puts "bal on the card is #{card.balance}"
 puts "top up card by: #{card.top_up(30)}"
+puts "deduct card by: #{card.deduct(4.5)}"
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,4 +14,10 @@ describe Oystercard do
       expect {subject.top_up(max_balance +1)}.to raise_error "Over maximum balance of #{max_balance}"
     end
   end
+  describe '#deduct'do
+    it 'subtracts journey fare from balance' do
+      subject.top_up(30)
+      expect {subject.deduct(4.5)}. to change{subject.balance}.by -4.5
+    end
+  end
 end
